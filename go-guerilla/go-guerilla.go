c80efe522ea9c8396a5ebfec070eec8acbb68348
Andrew Tynefield <andrew.tynefield@networkboxusa.com>
2024-05-29 11:24:19 -0500

    Add es

--- go-guerilla/go-guerilla.go
+++ go-guerilla/go-guerilla.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/joho/godotenv"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -20,11 +23,14 @@ type StoreConfig struct {
 	AccessKey   string `json:"access_key"`
 	SecretKey   string `json:"secret_key"`
 	EndpointURL string `json:"endpoint_url"`
+	EsURL       string `json:"es_url"`
+	EsApiKey    string `json:"es_api_key"`
 }
 
 var StoreProcessor = func() backends.Decorator {
 	var storeConfig *StoreConfig
 	var minioClient *minio.Client
+	var esClient *elasticsearch.Client
 
 	backends.Svc.AddInitializer(backends.InitializeWith(func(backendConfig backends.BackendConfig) error {
 		configType := backends.BaseConfig(&StoreConfig{})
@@ -33,6 +39,24 @@ var StoreProcessor = func() backends.Decorator {
 			return err
 		}
 		storeConfig = bcfg.(*StoreConfig)
+
+		esCfg := elasticsearch.Config{
+			Addresses: []string{storeConfig.EsURL},
+			APIKey:    storeConfig.EsApiKey,
+		}
+
+		esClient, err = elasticsearch.NewClient(esCfg)
+		if err != nil {
+			backends.Log().WithError(err).Error(err, " Unable to connect to Elasticsearch: ", esClient)
+		} else {
+			es, err := esClient.Info()
+			if err != nil {
+				backends.Log().WithError(err).Error(err, " Unable to get Elasticsearch info: ", es)
+			} else {
+				backends.Log().Info("Elasticsearch: ", es)
+			}
+		}
+
 		minioClient, err = minio.New(storeConfig.EndpointURL, &minio.Options{
 			Creds:  credentials.NewStaticV4(storeConfig.AccessKey, storeConfig.SecretKey, ""),
 			Secure: false,
@@ -44,7 +68,7 @@ var StoreProcessor = func() backends.Decorator {
 		if err != nil {
 			backends.Log().WithError(err).Error(err, " Unable to PUT TESTPUT object; minio: ", res)
 		}
-		backends.Log().Info("Test Object PUT")
+		backends.Log().Info("Test Object PUT ", res)
 
 		return nil
 	}))
@@ -65,6 +89,46 @@ var StoreProcessor = func() backends.Decorator {
 					// if no error:
 					return p.Process(e, task)
 				} else if task == backends.TaskSaveMail {
+
+					type Envelope struct {
+						MailFrom string   `json:"mail_from"`
+						RcptTo   []string `json:"rcpt_to"`
+						Ehlo     string   `json:"ehlo"`
+						RemoteIp string   `json:"remote_ip"`
+						TLS      bool     `json:"tls"`
+						QueuedId string   `json:"queued_id"`
+					}
+					type Document struct {
+						Envelope Envelope            `json:"envelope"`
+						Headers  map[string][]string `json:"headers"`
+						StoreUrl string              `json:"store_url"`
+						StoredIn time.Duration       `json:"stored_in"`
+						Date     time.Time           `json:"date"`
+					}
+
+					document := Document{}
+					document.Headers = make(map[string][]string)
+
+					interestingHeaders := []string{
+						"From",
+						"To",
+						"Subject",
+						"Date",
+						"Message-ID",
+						"Message-Id",
+						"X-Mailer",
+						"In-Reply-To",
+						"DKIM-Signature",
+						"Mime-Version",
+						"Content-Type",
+					}
+
+					for _, header := range interestingHeaders {
+						if e.Header[header] != nil {
+							document.Headers[header] = e.Header.Values(header)
+						}
+					}
+
 					start := time.Now()
 
 					// PUT full message contents to minio
@@ -83,6 +147,59 @@ var StoreProcessor = func() backends.Decorator {
 					// call the next processor in the chain
 					elapsed := time.Since(start)
 					backends.Log().Debug("stored in: ", elapsed)
+
+					document.Envelope.MailFrom = e.MailFrom.String()
+					for _, rcpt := range e.RcptTo {
+						document.Envelope.RcptTo = append(document.Envelope.RcptTo, rcpt.String())
+					}
+					document.Envelope.Ehlo = e.Helo
+					document.Envelope.RemoteIp = e.RemoteIP
+					document.Envelope.TLS = e.TLS
+					document.Envelope.QueuedId = e.QueuedId
+					document.StoreUrl = res.Location
+					document.StoredIn = elapsed
+					document.Date = time.Now()
+
+					esdata, _ := json.Marshal(document)
+					esClient.Index("messages", bytes.NewReader(esdata))
+
+					return p.Process(e, task)
+				} else {
+					return p.Process(e, task)
+				}
+			},
+		)
+	}
+}
+
+var DkimProcessor = func() backends.Decorator {
+	return func(p backends.Processor) backends.Processor {
+		return backends.ProcessWith(
+			func(e *mail.Envelope, task backends.SelectTask) (backends.Result, error) {
+				if task == backends.TaskValidateRcpt {
+
+					// if you want your processor to validate recipents,
+					// validate recipient by checking
+					// the last item added to `e.RcptTo` slice
+					// if error, then return something like this:
+					/* return backends.NewResult(
+					   response.Canned.FailNoSenderDataCmd),
+					   backends.NoSuchUser
+					*/
+					// if no error:
+					return p.Process(e, task)
+				} else if task == backends.TaskSaveMail {
+					start := time.Now()
+
+					dkimSignature := e.Header.Get("DKIM-Signature")
+					if dkimSignature != "" {
+						// verify DKIM signature
+						backends.Log().Debug("DKIM-Signature: ", dkimSignature)
+						// perform operations with dkimSignature
+					}
+
+					elapsed := time.Since(start)
+					backends.Log().Debug("dkim in: ", elapsed)
 					return p.Process(e, task)
 				}
 				return p.Process(e, task)
@@ -104,18 +221,21 @@ func main() {
 	}
 	cfg.Servers = append(cfg.Servers, sc)
 	bcfg := backends.BackendConfig{
-		"save_workers_size":  4,
+		"save_workers_size":  1,
 		"save_process":       "HeadersParser|Header|Hasher|Store",
 		"log_received_mails": true,
 		"primary_mail_host":  "example.com",
 		"access_key":         os.Getenv("MINIO_ACCESS_KEY"),
 		"secret_key":         os.Getenv("MINIO_SECRET_KEY"),
 		"endpoint_url":       os.Getenv("MINIO_ENDPOINT"),
+		"es_url":             os.Getenv("ES_URL"),
+		"es_api_key":         os.Getenv("ES_API_KEY"),
 	}
 	cfg.BackendConfig = bcfg
 
 	d := guerrilla.Daemon{Config: cfg}
 	d.AddProcessor("Store", StoreProcessor)
+	d.AddProcessor("Dkim", DkimProcessor)
 
 	err = d.Start()
 	if err == nil {
--- go.mod
+++ go.mod
@@ -14,7 +14,11 @@ require (
 require (
 	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/elastic/elastic-transport-go/v8 v8.5.0 // indirect
+	github.com/elastic/go-elasticsearch/v8 v8.13.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.6 // indirect
@@ -23,6 +27,9 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/rs/xid v1.5.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/net v0.23.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
